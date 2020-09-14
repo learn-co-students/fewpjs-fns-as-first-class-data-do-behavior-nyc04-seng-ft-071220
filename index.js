1e64c8b4c43a3d48ebc10977c39cca7fbafb8aae
Dfelix02 <david.felix02@hotmail.com>
2020-09-13 20:17:29 -0400

    some of the greet function/ mac console bugging out

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(hour_time) {
+  if (hour_time<1200){
+    return console.log("Good Morning")
+  }
+  else if(hour_time>1700){
+    return console.log("Good Evening")
+  }
+  else {
+    return console.log("Good Afternoon")
+  }
+}
+function displayMessage() {
+
+}
