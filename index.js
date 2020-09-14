9d435200eef71462030f5bb06b698be30eefdfe1
hortencia718 <hortenciaecisneros@gmail.com>
2020-09-14 15:24:01 -0400

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  displayMessage(greet(Strtime))
 }
 
 /* End Given Code, don't edit above here...*/
-
+function displayMessage(message){
+  document.getElementById('greeting').innerText = message;
+}
+/* get the document --> get element by its id in this cause is the greeting
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+ function greet(Strtime,Strmin){
+   const hour = parseInt(Strtime, 10);
+   if (hour < 12) return "Good Morning"
+   if (hour > 18) return "Good Evening"
+   return "Good Afternoon"
+ }
